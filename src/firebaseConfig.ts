b2f6800fc9f3fb9722369c207ef89dbda9eb0723
arturo378 <amadrid378@hotmail.com>
2021-02-16 16:00:07 -0600

    removed authentication tokens

--- src/firebaseConfig.ts
+++ src/firebaseConfig.ts
@@ -2,16 +2,6 @@ import { resolve } from 'dns'
 import * as firebase from 'firebase'
 
 
-const config = {
-    apiKey: "AIzaSyD4RlNnKl7zHleWWpf3aI8vGI4vAmDNAzI",
-    authDomain: "thesis-6c5d6.firebaseapp.com",
-    databaseURL: "https://thesis-6c5d6.firebaseio.com",
-    projectId: "thesis-6c5d6",
-    storageBucket: "thesis-6c5d6.appspot.com",
-    messagingSenderId: "1019570880262",
-    appId: "1:1019570880262:web:09d97e96a80524007b2887",
-    measurementId: "G-VCL7H5V0YC"
-}
 
 
 
